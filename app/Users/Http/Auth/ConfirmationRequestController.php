f0c42c508263901d888b9d60151fd026092f6faa
Thomas Warlop <thomas.warlop@gmail.com>
2015-05-07 17:10:25 +0200

    delete confirmation request controller
    Signed-off-by: Thomas Warlop <thomas.warlop@gmail.com>

--- app/Users/Http/Auth/ConfirmationRequestController.php
+++ app/Users/Http/Auth/ConfirmationRequestController.php
@@ -1,14 +1,5 @@
 <?php namespace App\Users\Http\Auth;
 
-/**
- * This class will allow a user to request a new confirmation email
- * Not the most used feature, but a rather important considering user friendlyness.
- * Signin up is an important step and should work without a hassle.
- *
- * Class ConfirmationRequestController
- *
- * @package App\Users\Http\Auth
- */
 class ConfirmationRequestController extends Controller
 {
     public function getConfirmation()
