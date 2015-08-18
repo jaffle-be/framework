46fcdbc525f01f96798f26e84eabde0d2bded769
vagrant <thomas.warlop@gmail.com>
2015-08-18 09:54:11 +0000

    fix migrations for menu

---
+++ database/migrations/2015_05_20_134054_create_account_contact_information_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateAccountContactInformationTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account_contact_information', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('account_id', false, true);
+            $table->foreign('account_id', 'account_contact_information_to_account')->references('id')->on('accounts')->onDelete('cascade');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('vat');
+            $table->string('website');
+            $table->string('hours');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('account_contact_information', function (Blueprint $table) {
+            $table->dropForeign('account_contact_information_to_account');
+        });
+    }
+
+}
