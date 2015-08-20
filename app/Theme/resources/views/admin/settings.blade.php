951dff3ad076e142e86af7a8340127623b818320
Thomas Warlop <thomas.warlop@gmail.com>
2015-08-20 14:05:35 +0200

    theme languages and layout fix

--- app/Theme/resources/views/admin/settings.blade.php
+++ app/Theme/resources/views/admin/settings.blade.php
@@ -1,40 +1,50 @@
 <div class="wrapper-content" ng-controller="ThemeController as vm" ng-init="vm.options = {{ system_options() }}">
 
-    <div class="ibox">
+    <div class="row">
 
-        <div class="ibox-title">
-            <h5>{{ Lang::get('theme::settings.theme-selection') }}</h5>
-        </div>
+        <div class="ibox">
 
-        <div class="ibox-content">
-
-            <div class="alert alert-danger" ng-show="vm.failed">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                <strong>{{ Lang::get('theme::settings.error') }}</strong> {{ Lang::get('theme::settings.something-bad-happened') }}
+            <div class="ibox-title">
+                <h5>{{ Lang::get('theme::admin.theme-selection') }}</h5>
             </div>
 
-            <div class="col-lg-offset-1 col-lg-4 text-center" style="padding-top: 40px">
-                <div class="form-group">
+            <div class="ibox-content">
 
-                    <label class="control-label" for="theme">{{ Lang::get('theme::settings.active-theme') }}</label>
-                    <select class="form-control" ng-model="vm.theme" ng-options="theme.name for theme in vm.themes"></select>
+                <div class="alert alert-danger" ng-show="vm.failed">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <strong>{{ Lang::get('theme::settings.error') }}</strong> {{ Lang::get('theme::admin.something-bad-happened') }}
                 </div>
 
-                <div class="form-group">
-                    <button class="btn btn-warning" ng-click="vm.activate()">{{ Lang::get('theme::settings.change-theme') }}</button>
+                <div class="row">
+
+                    <div class="col-lg-offset-1 col-lg-4 text-center" style="padding-top: 40px">
+                        <div class="form-group">
+
+                            <label class="control-label" for="theme">{{ Lang::get('theme::admin.active-theme') }}</label>
+                            <select class="form-control" ng-model="vm.theme" ng-options="theme.name for theme in vm.themes"></select>
+                        </div>
+
+                        <div class="form-group">
+                            <button class="btn btn-warning" ng-click="vm.activate()">{{ Lang::get('theme::admin.change-theme') }}</button>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-lg-offset-1">
+
+                        <label class="control-label">{{ Lang::get('theme::admin.logo') }}</label>
+
+                        <image-input owner-type="'account-logo'" owner-id="{{ $account->id }}" locale="vm.options.locale" limit="1"></image-input>
+                    </div>
+
                 </div>
-            </div>
 
-            <div class="col-lg-6 col-lg-offset-1">
-                <image-input owner-type="'account-logo'" owner-id="{{ $account->id }}" locale="vm.options.locale" limit="1"></image-input>
-            </div>
+                <div class="clearfix"></div>
 
-            <div class="clearfix"></div>
+            </div>
 
         </div>
 
     </div>
-
 
     <div class="row" ng-show="vm.theme">
 
