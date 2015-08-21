65533801115f4aab3d6ade0c10acf361cbb9b820
Thomas Warlop <thomas.warlop@gmail.com>
2015-08-21 18:06:26 +0200

    improve seeder for images

--- app/System/Seeder.php
+++ app/System/Seeder.php
@@ -1,11 +1,14 @@
 <?php
 namespace App\System;
 
+use App\Media\ImageDimensionHelpers;
 use Faker\Factory as Faker;
 use Illuminate\Database\Seeder as BaseSeeder;
 
 abstract class Seeder extends BaseSeeder
 {
+    use ImageDimensionHelpers;
+
     /**
      * The Faker instance.
      *
@@ -34,6 +37,45 @@ abstract class Seeder extends BaseSeeder
             $this->de = Faker::create('de_DE');
             $this->faker = $this->en;
         }
+    }
+
+    /**
+     * @param $images
+     * @param $sizes
+     *
+     * @return mixed
+     */
+    protected function preImageCaching()
+    {
+        //run images cachings.
+        foreach ($this->image_names as $image) {
+
+            $path = $this->prefix . $image;
+
+            foreach ($this->image_sizes as $size) {
+
+                list($width, $height) = $this->dimensions($size);
+
+                $constraint = $this->constraint($width, $height);
+
+                $this->images->cache(function ($image) use ($path, $width, $height, $constraint) {
+                    $image->make($path)->resize($width, $height, $constraint);
+                });
+            }
+        }
+    }
+
+    protected function newImage($owner, $account)
+    {
+        $image = rand(0, count($this->image_names) - 1);
+
+        $this->dispatchFromArray(StoreNewImage::class, [
+            'account' => $account,
+            'owner'   => $owner,
+            'path'    => $this->prefix . $this->image_names[$image],
+            'sizes'   => $this->image_sizes,
+            'seeding' => true
+        ]);
     }
 
 }
