0cc4245503159a126f602fc77af44e5420a9955f
Thomas Warlop <thomas.warlop@gmail.com>
2015-08-25 15:19:10 +0200

    fix showing locale tabs

--- app/System/resources/views/admin/locale-tabs.blade.php
+++ app/System/resources/views/admin/locale-tabs.blade.php
@@ -1,5 +1,4 @@
-<tabset ng-show="vm.options.locales.length">
-
+<tabset ng-show="vm.options.locales.length != 0">
     <tab ng-repeat="locale in vm.options.locales" heading="@{{ locale.slug }}" {{ isset($clickRefresh) && $clickRefresh ? 'st-click-refresh' : '' }} active="locale.active" select="vm.options.locale = locale.slug">
 
     </tab>
