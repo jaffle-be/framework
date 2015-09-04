5d8919a744040ca4eff27decf78b5c96ecf736ae
Thomas Warlop <thomas.warlop@gmail.com>
2015-09-04 10:14:03 +0200

    improve invitation mail

--- app/Account/resources/views/admin/members/invitation/email.blade.php
+++ app/Account/resources/views/admin/members/invitation/email.blade.php
@@ -2,16 +2,47 @@
 
 @section('content')
 
-    <div class="container">
+    <div style="height:15px">&nbsp;</div><!-- divider -->
 
-        <div class="content-sm text-center">
+        <!--Start Discount -->
+<table width="700" border="0" cellpadding="0" cellspacing="0" align="center" class="deviceWidth">
+    <tr>
+        <td width="100%" bgcolor="#ffffff">
+            <!-- Left Box  -->
+            <table width="70%" border="0" cellpadding="0" cellspacing="0" align="left" class="deviceWidth">
+                <tr>
+                    <td class="center">
+                        <table border="0" cellpadding="0" cellspacing="0" align="center">
+                            <tr>
+                                <td class="center" style="font-size: 16px; color: #687074; font-weight: bold; text-align: center; font-family: Arial, Helvetica, sans-serif; line-height: 25px; vertical-align: middle; padding: 20px 10px 10px;">
+                                    {{ Lang::get('account::admin.users.invitation-text') }}
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <!--End Left Box-->
+            <!--Right Box-->
+            <table width="30%" border="0" cellpadding="0" cellspacing="0" align="center" class="deviceWidth">
+                <tr>
+                    <td style=" padding: 15px 20px 30px;">
+                        <table align="center">
+                            <tr>
+                                <td valign="top" style="padding: 7px 15px; text-align: center; background-color: #74C52C;" class="center">
+                                    <a style="color: #fff; font-size: 12px; font-weight: bold; text-decoration: none; font-family: Arial, sans-serif; text-alight: center;" href="{{ route('store.auth.invitation.show', [$invitation->token]) }}">{{ Lang::get('account::admin.users.accept') }}</a>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <!--End Right Box-->
+        </td>
+    </tr>
+</table>
+<!--End Discount -->
 
-            <p class="margin-bottom-40">{{ Lang::get('account::admin.users.invitation-text') }}</p>
-
-            <a class="btn btn-u btn-lg" href="{{ route('store.auth.invitation.show', $invitation->token) }}">{{ Lang::get('account::admin.users.accept') }}</a>
-
-        </div>
-
-    </div>
+<div style="height:15px">&nbsp;</div><!-- divider -->
 
 @stop
