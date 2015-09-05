d82b7d250f56b1db91a8f56c571f139b2ead077e
Thomas Warlop <thomas.warlop@gmail.com>
2015-09-05 11:24:45 +0200

    fix user seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -8,28 +8,12 @@ class UsersTableSeeder extends Seeder{
     public function run()
     {
         User::create([
-            'email' => 'thomas.warlop@gmail.com',
+            'email' => 'thomas@digiredo.be',
             'password' => \Hash::make('thomasthomas'),
             'firstname' => 'Thomas',
             'lastname' => 'Warlop',
             'confirmed' => 1
         ]);
-
-        $users = ['Ruud' => 'Van Der Heyden', 'Gauthier' => 'Geldhof', 'Igor' => 'Delameilleure'];
-
-
-        foreach($users as $firstname => $lastname)
-        {
-            User::create([
-                'email' => 'thomas.warlop@gmail.com',
-                'password' => \Hash::make('thomasthomas'),
-                'firstname' => $firstname,
-                'lastname' => $lastname,
-                'confirmed' => 1
-            ]);
-        }
     }
-
-
 
 }
