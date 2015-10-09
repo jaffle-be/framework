2a5b342ebd678417a23f5f1d0219d62bb36cd83f
Thomas Warlop <thomas.warlop@gmail.com>
2015-10-09 10:22:51 +0200

    cleanup config

--- config/media.php
+++ config/media.php
@@ -24,54 +24,6 @@ return [
         ]
     ],
 
-    /**
-     * HEADS UP:
-     * In the admin section we always need a 340 dimension in the detail pages (image uploader)
-     * And a 150 dimension in the overview pages
-     *
-     */
-    'sizes'  => [
-
-        //these values should be coming from the theme
-        //we should simply add the values used in the admin
-        'blog'         => [
-            '1140x',
-            '512x',
-            '460x',
-            '340x',
-            '150x',
-            '60x',
-        ],
-
-        'portfolio'    => [
-            '1280x',
-            '512x',
-            '340x',
-            '270x',
-            '150x',
-        ],
-
-        'user'         => [
-            '1280x',
-            '512x',
-            '340x',
-            '80x'
-        ],
-
-        'account-logo' => [
-            '512x',
-            '340x',
-            'x40'
-        ],
-
-        'client'       => [
-            '512x',
-            '340x',
-            'x40',
-            'x90',
-        ]
-    ],
-
     'videos' => [
         //these values should be coming from the theme
     ]
