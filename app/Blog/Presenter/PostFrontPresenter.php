4bedacc321d6a9a5efa8eff4c5b04501243ebcc6
Thomas Warlop <thomas.warlop@gmail.com>
2015-10-13 08:51:31 +0200

    refactor into 2 new traits: shortcode compiler and content presenter

--- app/Blog/Presenter/PostFrontPresenter.php
+++ app/Blog/Presenter/PostFrontPresenter.php
@@ -3,80 +3,15 @@
 use App\Media\Image;
 use App\Media\Shortcodes\MediaShortcodes;
 use App\System\Presenter\BasePresenter;
-use Markdown;
+use App\System\Presenter\ContentPresenterTrait;
+use App\System\Presenter\ShortCodeCompiler;
 
 class PostFrontPresenter extends BasePresenter
 {
+    protected $shortcodes = ['media'];
+
+    use ContentPresenterTrait;
+    use ShortCodeCompiler;
     use MediaShortcodes;
-
-    /**
-     * Returns the entire post, fully loaded with shortcodes
-     * Ready to be displayed onto your website.
-     *
-     * @return mixed
-     */
-    public function content()
-    {
-        $content = $this->entity->content;
-
-        //compile our custom shortcodes into valid markdown
-        //take note, in our system provider,
-        //we add the attributes extension to the
-        //markdown environment.
-        $content = $this->compileShortcodes($content);
-
-        return Markdown::convertToHtml($content);
-    }
-
-    /**
-     * The extract should not contain any style or media
-     * It should simply be a text snippet.
-     *
-     * Therefor, at the end, we always strip all tags
-     * We also strip out any shortcodes, which would only
-     * inject either media or custom marked up content.
-     *
-     * @return string
-     */
-    public function extract()
-    {
-        $content = $this->entity->content;
-
-        $content = $this->stripShortcodes($content);
-
-        $content = Markdown::convertToHtml($content);
-
-        $content = strip_tags($content);
-
-        return $this->snippet($content);
-    }
-
-    protected function snippet($str, $wordCount = 60) {
-        return implode(
-            '',
-            array_slice(
-                preg_split(
-                    '/([\s,\.;\?\!]+)/',
-                    $str,
-                    $wordCount*2+1,
-                    PREG_SPLIT_DELIM_CAPTURE
-                ),
-                0,
-                $wordCount*2-1
-            )
-        ) . '&nbsp;...';
-    }
-
-    protected function compileShortcodes($content)
-    {
-        $content = $this->compileMediaShortcodes($content);
-
-        return $content;
-    }
-
-    protected function stripShortcodes($content)
-    {
-        return $this->stripMediaShortcodes($content);
-    }
 
 }
---
+++ app/System/Presenter/ShortCodeCompiler.php
@@ -0,0 +1,32 @@
+<?php namespace App\System\Presenter;
+
+trait ShortCodeCompiler
+{
+    protected function compileShortcodes($content)
+    {
+        return $this->loopShortcodes('compile', $content);
+    }
+
+    protected function stripShortcodes($content)
+    {
+        return $this->loopShortcodes('strip', $content);
+    }
+
+    protected function loopShortcodes($type, $content)
+    {
+        if(!property_exists($this, 'shortcodes'))
+        {
+            throw new \Exception('You need to add the shortcodes property to ' . get_called_class());
+        }
+
+        foreach ($this->shortcodes as $code) {
+            $method = $type . ucfirst($code) . 'ShortCodes';
+
+            if (method_exists($this, $method)) {
+                $content = $this->$method($content);
+            }
+        }
+
+        return $content;
+    }
+}
