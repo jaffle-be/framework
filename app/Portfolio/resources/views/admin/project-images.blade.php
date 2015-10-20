d7348c373dd1be0770481577de8d8b97b5a39d48
Thomas Warlop <thomas.warlop@gmail.com>
2015-10-20 17:50:11 +0200

    improve the use of the media tabs

--- app/Portfolio/resources/views/admin/project-images.blade.php
+++ app/Portfolio/resources/views/admin/project-images.blade.php
@@ -1,15 +1,1 @@
-<div class="ibox">
-
-    <div class="ibox-title">
-        <h5>{{ Lang::get('portfolio::admin.project.images') }}</h5>
-    </div>
-
-    <div class="ibox-content">
-
-
-        <image-input owner-type="'portfolio'" owner-id="$state.params.id" locale="vm.options.locale"></image-input>
-
-
-    </div>
-
-</div>
+@include('media::admin.full', ['type' => 'portfolio'])
