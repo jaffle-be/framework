fc6fd1bfddadb44539c959856647b36c2dccc2b3
Thomas Warlop <thomas.warlop@gmail.com>
2015-10-20 14:55:31 +0200

    add some comments for the future :-)

--- app/Menu/MenuServiceProvider.php
+++ app/Menu/MenuServiceProvider.php
@@ -21,8 +21,16 @@ class MenuServiceProvider extends ServiceProvider
     protected function listeners()
     {
         $this->app->booted(function(){
+            //we might want to provide 5 menus instead of allow each theme to define it's own menus.
+            //a theme can simply decide to hook into them or not.
+            //maybe you could also register a 'dynamic' menu, to load in the sidebar
+            //the items it shows could for example be our documentation?
             $this->app['menu']->register('primary menu');
+            //$this->app['menu']->register('secondary menu');
             $this->app['menu']->register('footer menu');
+            //$this->app['menu']->register('shopping menu');
+            //$this->app['menu']->register('shopping secondary menu');
+
         });
 
         $this->app['events']->listen('eloquent.saved:*', 'App\Menu\SavedMenuHookable');
