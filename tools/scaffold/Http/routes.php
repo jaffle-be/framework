93a960ab62e881ad415fc9c5e58ed920648d5b6c
Thomas Warlop <thomas.warlop@gmail.com>
2015-10-21 11:28:54 +0200

    improve scaffolding

--- tools/scaffold/Http/routes.php
+++ tools/scaffold/Http/routes.php
@@ -4,21 +4,37 @@
  * store controllers
  */
 Route::group([
-    'namespace' => 'App\Blog\Http\Admin'
+    'namespace' => 'App\Blog\Http',
+    'as' => 'store.'
 ], function () {
 
-    Route::group(['prefix' => 'templates/admin'], function () {
-        //template files, load at top so we do not end up in the show method
-//        Route::get('blog/overview', 'BlogController@overview');
-//        Route::get('blog/detail', 'BlogController@detail');
+    Route::group([
+        'namespace' => 'Admin',
+    ], function()
+    {
+        Route::group(['prefix' => 'templates/admin'], function () {
+            //template files, load at top so we do not end up in the show method
+            Route::get('blog/overview', 'BlogController@overview');
+            Route::get('blog/detail', 'BlogController@detail');
+        });
+
+        //actual resource
+        Route::group(['prefix' => 'api/admin'], function () {
+            Route::resource('blog', 'BlogController');
+            Route::post('blog/batch-delete', 'BlogController@batchDestroy');
+        });
     });
 
-    //actual resource
-    Route::group(['prefix' => 'api/admin'], function () {
-        Route::resource('blog', 'BlogController', ['as' => 'store', 'only' => ['index', 'show', 'store', 'update']]);
-    });
-});
+    //FRONT ROUTES
+    if(env('APP_MULTIPLE_LOCALES'))
+    {
+        foreach(config('system.locales') as $locale)
+        {
+            Route::get("$locale/blog", ['uses' => 'BlogController@index', 'as' => "$locale.blog.index"]);
+        }
+    }
+    else{
+        Route::get('blog', ['uses' => 'BlogController@index', 'as' => 'blog.index']);
+    }
 
-Route::group(['namespace' => 'App\Blog\Http'], function () {
-//    Route::resource('blog', 'BlogController', ['as' => 'store', 'only' => ['index', 'show']]);
 });
