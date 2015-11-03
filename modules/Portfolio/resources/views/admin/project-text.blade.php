0cd22f14be25879cf4d9de67fb5457616d3aedaa
Thomas Warlop <thomas.warlop@gmail.com>
2015-11-03 11:22:56 +0100

    fix datepicker

--- modules/Portfolio/resources/views/admin/project-text.blade.php
+++ modules/Portfolio/resources/views/admin/project-text.blade.php
@@ -30,7 +30,7 @@
 
                     <div class="input-group datepicker">
 
-                        <input datepicker-popup="dd/MM/yyyy" show-weeks="false" is-open="status.datepickerStatus" type="text" class="form-control" ng-model="vm.project.date" ng-click="vm.openDatepicker($event)" ng-change="vm.save()"/>
+                        <input uib-datepicker-popup="dd/MM/yyyy" show-weeks="false" is-open="status.datepickerStatus" type="text" class="form-control" ng-model="vm.project.date" ng-click="vm.openDatepicker($event)" ng-change="vm.save()"/>
 
                         <div class="input-group-btn">
                                 <span type="button" class="btn btn-default" ng-click="vm.openDatepicker($event)">
