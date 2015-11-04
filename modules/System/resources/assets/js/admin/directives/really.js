e2bdbf475aa41bde7b02f17cdb52134986d21ca3
Thomas Warlop <thomas.warlop@gmail.com>
2015-11-04 07:16:12 +0100

    fix confirm and really injections

--- modules/System/resources/assets/js/admin/directives/really.js
+++ modules/System/resources/assets/js/admin/directives/really.js
@@ -1,63 +1,68 @@
 (function () {
     'use strict';
 
-    angular.module('system').directive('ngReally', ['$uibModal', '$translate',
-        function ($uibModal, $translate) {
+    angular.module('system').directive('ngReally', really);
 
-            var ModalInstanceCtrl = function ($scope, $uibModalInstance) {
-                $scope.ok = function () {
-                    $uibModalInstance.close();
-                };
+    really.$inject = ['$uibModal', '$translate'];
 
-                $scope.cancel = function () {
-                    $uibModalInstance.dismiss('cancel');
-                };
+    function really($uibModal, $translate) {
+
+        ModalInstanceCtrl.$inject = ['$scope', '$uibModalInstance'];
+
+        function ModalInstanceCtrl($scope, $uibModalInstance) {
+            $scope.ok = function () {
+                $uibModalInstance.close();
             };
 
-            return {
-                restrict: 'A',
-                scope: {
-                    ngReally: "&",
-                },
-                link: function (scope, element, attrs) {
-                    element.bind('click', function () {
+            $scope.cancel = function () {
+                $uibModalInstance.dismiss('cancel');
+            };
+        };
 
-                        $translate('CONFIRM').then(function (translation) {
+        return {
+            restrict: 'A',
+            scope: {
+                ngReally: "&",
+            },
+            link: function (scope, element, attrs) {
+                element.bind('click', function () {
 
-                            if (!scope.ngReallyMessage)
+                    $translate('CONFIRM').then(function (translation) {
+
+                        if (!scope.ngReallyMessage)
+                        {
+                            if (translation)
                             {
-                                if (translation)
-                                {
-                                    scope.ngReallyMessage = translation;
-                                }
-                                else
-                                {
-                                    scope.ngReallyMessage = 'Are you sure?';
-                                }
+                                scope.ngReallyMessage = translation;
                             }
+                            else
+                            {
+                                scope.ngReallyMessage = 'Are you sure?';
+                            }
+                        }
 
-                            var modalHtml = '<div class="modal-body">' + scope.ngReallyMessage + '</div>';
-                            modalHtml += '<div class="modal-footer"><button class="btn btn-danger" ng-click="ok()">{{ "REMOVE" | translate }}</button><button class="btn btn-default" ng-click="cancel()">{{ "CANCEL" | translate }}</button></div>';
+                        var modalHtml = '<div class="modal-body">' + scope.ngReallyMessage + '</div>';
+                        modalHtml += '<div class="modal-footer"><button class="btn btn-danger" ng-click="ok()">{{ "REMOVE" | translate }}</button><button class="btn btn-default" ng-click="cancel()">{{ "CANCEL" | translate }}</button></div>';
 
-                            var modalInstance = $uibModal.open({
-                                template: modalHtml,
-                                controller: ModalInstanceCtrl,
-                                animation: false,
-                                animate: false
-                            });
+                        var modalInstance = $uibModal.open({
+                            template: modalHtml,
+                            controller: ModalInstanceCtrl,
+                            animation: false,
+                            animate: false
+                        });
 
-                            modalInstance.result.then(function () {
-                                scope.ngReally();
-                            }, function () {
-                                //Modal dismissed
-                            });
-
+                        modalInstance.result.then(function () {
+                            scope.ngReally();
+                        }, function () {
+                            //Modal dismissed
                         });
 
                     });
 
-                }
+                });
+
             }
-        }]);
+        }
+    }
 
 })();
