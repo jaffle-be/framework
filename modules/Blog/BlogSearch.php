373db3571c865d9100fc211b883c5ee14fbeee5e
Thomas Warlop <thomas.warlop@gmail.com>
2015-11-09 13:30:18 +0100

    make search queries part of their specific trait

---
+++ modules/Blog/BlogSearch.php
@@ -0,0 +1,58 @@
+<?php namespace Modules\Blog;
+
+use Illuminate\Http\Request;
+use Modules\Account\AccountManager;
+
+trait BlogSearch
+{
+
+    protected function postsQuery(Request $request, AccountManager $manager, $locale)
+    {
+        return [
+            'index' => 'framework',
+            'type'  => 'posts',
+            'body'  => [
+                "query" => [
+                    "filtered" => [
+                        "query"  => [
+                            "nested" => [
+                                "path"  => "translations.$locale",
+                                "query" => [
+                                    "multi_match" => [
+                                        "query"  => $request->get('query'),
+                                        "fields" => ["translations.$locale.title", "translations.$locale.content", "translations.$locale.extract"]
+                                    ]
+                                ]
+                            ]
+                        ],
+                        "filter" => [
+                            "bool" => [
+                                "must" => [
+                                    [
+                                        "term" => [
+                                            "account_id" => $manager->account()->id
+                                        ]
+                                    ],
+                                    [
+                                        "nested" => [
+                                            "path"  => "translations.$locale",
+                                            "query" => [
+                                                "range" => [
+                                                    "translations.$locale.publish_at" => [
+                                                        "lte" => "now"
+                                                    ]
+                                                ]
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+    }
+
+
+}
