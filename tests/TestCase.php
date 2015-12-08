4c931e0f9563f8b0fe66fe2435fc2a6150ba98f5
Thomas Warlop <thomas.warlop@gmail.com>
2015-12-08 10:38:09 +0100

    add most basic testcase

---
+++ tests/TestCase.php
@@ -0,0 +1,30 @@
+<?php namespace Test;
+
+use App\Console\Kernel;
+
+class TestCase extends \Illuminate\Foundation\Testing\TestCase
+{
+
+    protected $baseUrl = 'http://digiredo.local';
+
+    public function setUp()
+    {
+        putenv('RUNNING_TESTS_FRONT=false');
+
+        parent::setUp();
+    }
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+}
