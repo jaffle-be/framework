62a60bf8b7d30802c6975b1eb7bf4f814c5c56a9
Thomas Warlop <thomas.warlop@gmail.com>
2015-12-14 09:26:28 +0100

    only set account to the email jobs

--- app/Jobs/EmailJob.php
+++ app/Jobs/EmailJob.php
@@ -4,11 +4,14 @@ use Illuminate\Contracts\Bus\SelfHandling;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Modules\Account\AccountManager;
 
 class EmailJob extends Job implements SelfHandling, ShouldQueue
 {
 
     use InteractsWithQueue, SerializesModels;
+
+    protected $account;
 
     protected $root_url;
 
@@ -20,15 +23,16 @@ class EmailJob extends Job implements SelfHandling, ShouldQueue
 
     public function __construct()
     {
+        $this->account = app(AccountManager::class)->account();
         $this->root_url = config('app.url');
         $this->email_from = config('mail.from.address');
         $this->email_from_name = config('mail.from.name');
-        parent::__construct();
     }
 
     protected function baseData()
     {
         return array_merge(parent::baseData(), [
+            'account' => $this->account,
             'email_from' => $this->email_from,
             'email_from_name' => $this->email_from_name,
             'root_url' => $this->root_url,
--- app/Jobs/Job.php
+++ app/Jobs/Job.php
@@ -1,21 +1,6 @@
 <?php namespace App\Jobs;
 
-use Modules\Account\AccountManager;
-
 abstract class Job {
 
-    protected $account;
-
-    public function __construct()
-    {
-        $this->account = app(AccountManager::class)->account();
-    }
-
-    protected function baseData()
-    {
-        return [
-            'account' => $this->account
-        ];
-    }
 
 }
